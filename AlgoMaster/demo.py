2feb2dc64948ef9ec5512ef5c1b0516385d088ab
iamsajo <sajo2023b@mca.ajce.in>
2023-06-17 21:51:45 +0530

    first commit

---
+++ AlgoMaster/demo.py
@@ -0,0 +1,11 @@
+# create a new class for print hello world with funtion call
+
+class HelloWorld:
+    def __init__(self):
+        print("Hello World")
+
+    def printHelloWorld(self):
+        print("Hello World")
+    
+    def printHelloWorld2(self):
+        print("Hello World 2")
